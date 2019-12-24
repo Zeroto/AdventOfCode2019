01b358034f21c585fa29c18e2bdf5b729c720147
Sander Homan <sander.homan@wordtango.net>
2019-12-24 12:10:22 +0100

    Added day24 part 2

--- src/day24/day24.fs
+++ src/day24/day24.fs
@@ -21,27 +21,79 @@ let boardToBitmask width height (board: Tile array) =
         result <- result ||| bit
   result
 
-let evolve width height board =
-  board
-  |> Array.mapi (fun i s ->
-    let y,x = System.Math.DivRem(i,width)
-    let adjacentCount =
-      [(x-1,y); (x+1,y); (x,y-1); (x,y+1)]
-      |> List.filter (fun (nx,ny) -> nx>=0 && nx < width && ny>=0 && ny<height)
-      |> List.filter (fun (nx,ny) -> board.[nx+ny*width] = Closed)
-      |> List.length
-    match board.[x+y*width] with
-    | Open -> 
-      if adjacentCount = 1 || adjacentCount = 2 then
-        Closed
-      else
-        Open
-    | Closed ->
-      if adjacentCount = 1 then
-        Closed
-      else
-        Open
-  )
+let getAdjacentCoords (depth, x, y) =
+  let mutable result = []
+  if x = 0 then
+    result <- (depth-1, 1, 2) :: result
+  if x = 4 then
+    result <- (depth-1, 3, 2) :: result
+  if y = 0 then
+    result <- (depth-1, 2, 1) :: result
+  if y = 4 then
+    result <- (depth-1, 2, 3) :: result
+  
+  if x = 1 && y = 2 then
+    result <- (depth+1, 0, 0) :: (depth+1, 0, 1) :: (depth+1, 0, 2) :: (depth+1, 0, 3) :: (depth+1, 0, 4) :: result
+  if x = 3 && y = 2 then
+    result <- (depth+1, 4, 0) :: (depth+1, 4, 1) :: (depth+1, 4, 2) :: (depth+1, 4, 3) :: (depth+1, 4, 4) :: result
+  if x = 2 && y = 1 then
+    result <- (depth+1, 0, 0) :: (depth+1, 1, 0) :: (depth+1, 2, 0) :: (depth+1, 3, 0) :: (depth+1, 4, 0) :: result
+  if x = 2 && y = 3 then
+    result <- (depth+1, 0, 4) :: (depth+1, 1, 4) :: (depth+1, 2, 4) :: (depth+1, 3, 4) :: (depth+1, 4, 4) :: result
+
+  [(depth, x-1,y); (depth, x+1,y); (depth, x,y-1); (depth, x,y+1)]
+  |> List.filter (fun (_, nx,ny) -> nx>=0 && nx < 5 && ny>=0 && ny<5)
+  |> List.append result
+
+let emptyBoard = Array.create (5*5) Open
+
+let evolve width height boards =
+  let evolveBoard depth board =
+    board
+      |> Array.mapi (fun i t ->
+        let y,x = System.Math.DivRem(i,width)
+        if x = 2 && y = 2 then // center tile does not "exist"
+          Open
+        else
+          let adjacentCoords = getAdjacentCoords (depth, x, y)
+
+          let adjacentCount =
+            adjacentCoords
+            |> List.filter (fun (d,nx,ny) ->
+              let board = Map.tryFind d boards |> Option.defaultValue emptyBoard
+              board.[nx+ny*width] = Closed
+            )
+            |> List.length
+          match t with
+          | Open -> 
+            if adjacentCount = 1 || adjacentCount = 2 then
+              Closed
+            else
+              Open
+          | Closed ->
+            if adjacentCount = 1 then
+              Closed
+            else
+              Open
+      )
+
+  let mutable newBoards =
+    boards
+    |> Map.map evolveBoard
+
+  let minDepth = newBoards |> Map.toSeq |> Seq.minBy (fun (k,_) -> k) |> fst
+  let maxDepth = newBoards |> Map.toSeq |> Seq.maxBy (fun (k,_) -> k) |> fst
+
+  let minDepthBoard = evolveBoard (minDepth-1) emptyBoard
+  let maxDepthBoard = evolveBoard (maxDepth+1) emptyBoard
+
+  if minDepthBoard |> Array.exists (fun x -> x = Closed) then
+    newBoards <- Map.add (minDepth-1) minDepthBoard newBoards
+
+  if maxDepthBoard |> Array.exists (fun x -> x = Closed) then
+    newBoards <- Map.add (maxDepth+1) maxDepthBoard newBoards
+
+  newBoards
 
 let drawBoard width height (board: Tile array) =
   for y in 0 .. height-1 do
@@ -52,6 +104,15 @@ let drawBoard width height (board: Tile array) =
       | Closed -> printf "#"
     printf "\n"
   printf "\n"
+
+let drawBoards iteration (boards: Map<int, Tile array>) =
+  printfn "####### Iteration %d #######" iteration
+  boards 
+  |> Map.toSeq
+  |> Seq.iter (fun (d,b) ->
+    printfn "Depth: %d" d
+    drawBoard 5 5 b
+  )
 
 [<EntryPoint>]
 let main argv =
@@ -71,20 +132,21 @@ let main argv =
         |> Seq.toArray
       )
 
-  let seenBoards = HashSet<int>()
+  let recursiveBoards =
+    Map.empty
+    |> Map.add 0 board
 
-  let rec findFirstDuplicate board =
-    let newBoard = evolve width height board
-    drawBoard width height newBoard
-    let newMask = boardToBitmask width height newBoard
-    if seenBoards.Contains newMask then
-      newBoard
+  let rec findFirstDuplicate boards currentMinDepth currentMaxDepth iteration =
+    drawBoards iteration boards
+    if iteration = 200 then
+      boards
     else
-      seenBoards.Add newMask |> ignore
-      findFirstDuplicate newBoard
-
-  let dupBoard = findFirstDuplicate board
+      let newBoards = evolve 5 5 boards
+      findFirstDuplicate newBoards currentMinDepth currentMaxDepth (iteration+1)
 
 
-  printfn "%A" (boardToBitmask width height dupBoard)
+  let endBoard = findFirstDuplicate recursiveBoards 0 0 0
+
+
+  printfn "%A" (endBoard |> Map.toSeq |> Seq.sumBy (fun (d, b) -> b |> Array.sumBy (fun t -> if t = Closed then 1 else 0)))
   0 // return an integer exit code
